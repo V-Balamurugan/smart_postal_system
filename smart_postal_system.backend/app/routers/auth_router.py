551cf915141cb4028276ad510e0d3060aebb9f71
V-Balamurugan <balamurugankumar880@gmail.com>
2026-07-04 11:38:14 +0530

    login authentication

--- smart_postal_system.backend/app/routers/auth_router.py
+++ smart_postal_system.backend/app/routers/auth_router.py
@@ -1,6 +1,56 @@
-from fastapi import APIRouter
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db
+from app.models.user_model import User
+from app.schemas.auth_schema import LoginRequest, Token
+from app.core.security import (
+    verify_password,
+    create_access_token
+)
+from app.core.config import settings
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
+
+
+@router.post("/login", response_model=Token)
+def login(
+    request: LoginRequest,
+    db: Session = Depends(get_db)
+):
+
+    user = db.query(User).filter(
+        User.email == request.email
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        request.password,
+        user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={"sub": user.email},
+        expires_delta=timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
--- smart_postal_system.backend/main.py
+++ smart_postal_system.backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers import user_route as users
 from app.database.database import Base, engine
 from app.models.user_model import User
+from app.routers import auth_router as auth
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
@@ -10,7 +11,7 @@ version="1.0.0",
 description="API for the Smart Postal System application"
 )
 app.include_router(users.router)
-
+app.include_router(auth.router)
 @app.get("/hello")
 def home():
     return {"message": "Welcome to the Smart Postal System API!"}
